988ebc89746322ca61cd38889f1dd65ff7e26bfc
André M. Teixeira <andremarcondesteixeira@gmail.com>
2022-03-25 11:31:19 -0300

    starts a new Spring Boot project

---
+++ src/main/java/org/zew/donations/DonationsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.zew.donations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zew/donations/DonationsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zew.donations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
