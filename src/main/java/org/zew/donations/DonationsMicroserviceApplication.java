bcaec737f225b817929ba36a12651df687bd3982
André Teixeira <andremarcondesteixeira@gmail.com>
2022-04-09 21:53:20 -0300

    configuring Apache Kafka with Streams API (wip)

--- src/main/java/org/zew/donations/DonationsMicroserviceApplication.java
+++ src/main/java/org/zew/donations/DonationsMicroserviceApplication.java
@@ -2,12 +2,20 @@ package org.zew.donations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DonationsMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DonationsMicroserviceApplication.class, args);
+	}
+
+	@GetMapping(value="/")
+	public String getMethodName() {
+		return "Teste";
 	}
 
 }
---
+++ src/main/java/org/zew/donations/kafka/Configuration.java
@@ -0,0 +1,37 @@
+package org.zew.donations.kafka;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.apache.kafka.streams.StreamsConfig.APPLICATION_ID_CONFIG;
+import static org.apache.kafka.streams.StreamsConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.streams.StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG;
+import static org.apache.kafka.streams.StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG;
+
+@org.springframework.context.annotation.Configuration
+@EnableKafka
+@EnableKafkaStreams
+public class Configuration {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    public KafkaStreamsConfiguration getKafkaStreamsConfiguration() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(APPLICATION_ID_CONFIG, "zew-donations");
+        props.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        return new KafkaStreamsConfiguration(props);
+    }
+
+}
