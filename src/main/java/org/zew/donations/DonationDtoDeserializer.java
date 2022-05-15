45584930175ffbd978e059bc99ccc447fc529e1c
André Teixeira <andremarcondesteixeira@gmail.com>
2022-05-15 17:59:18 -0300

    mixes configuration and kafka listener into DonationsMicroserviceApplication

--- src/main/java/org/zew/donations/DonationDtoDeserializer.java
+++ src/main/java/org/zew/donations/DonationDtoDeserializer.java
@@ -1,21 +1,17 @@
 package org.zew.donations;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Deserializer;
 
-import java.nio.charset.StandardCharsets;
-
 @Slf4j
 @NoArgsConstructor
 public class DonationDtoDeserializer implements Deserializer<DonationDto> {
-    private final ObjectMapper objectMapper = DonationObjectMapper.getInstance().getObjectMapper();
 
     @Override
     public DonationDto deserialize(String s, byte[] bytes) {
         try {
-            return objectMapper.readValue(new String(bytes, StandardCharsets.UTF_8), DonationDto.class);
+            return null;
         } catch (Exception e) {
             log.error("Unable to deserialize message {}", bytes, e);
             return null;
